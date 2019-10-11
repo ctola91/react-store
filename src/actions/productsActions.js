2e9a350e499765936d25e93c33271d255b335646
cmtp <christian.tola@hotmail.com>
2019-10-11 17:20:14 -0400

    Add delete and update actions

--- src/actions/productsActions.js
+++ src/actions/productsActions.js
@@ -6,6 +6,12 @@ import {
   ADD_PRODUCTS_REQUEST,
   ADD_PRODUCTS_SUCCESS,
   ADD_PRODUCTS_ERROR,
+  DELETE_PRODUCTS_REQUEST,
+  DELETE_PRODUCTS_SUCCESS,
+  DELETE_PRODUCTS_ERROR,
+  UPDATE_PRODUCTS_REQUEST,
+  UPDATE_PRODUCTS_SUCCESS,
+  UPDATE_PRODUCTS_ERROR,
 } from './actionTypes';
 import ProductService from '../services/ProductService';
 
@@ -31,5 +37,29 @@ export const addProduct = product => async dispatch => {
     // eslint-disable-next-line no-console
     console.log('AXIOS_ERROR', err.response);
     dispatch(error(ADD_PRODUCTS_ERROR));
+  }
+};
+
+export const deleteProduct = id => async dispatch => {
+  dispatch(request(DELETE_PRODUCTS_REQUEST));
+
+  try {
+    const response = await ProductService.deleteProduct(id);
+    dispatch(received(DELETE_PRODUCTS_SUCCESS, response.data));
+  } catch (err) {
+    console.log('AXIOS_ERROR', err.response);
+    dispatch(error(DELETE_PRODUCTS_ERROR));
+  }
+};
+
+export const updateProduct = product => async dispatch => {
+  dispatch(request(UPDATE_PRODUCTS_REQUEST));
+
+  try {
+    const response = await ProductService.updateProduct(product);
+    dispatch(received(UPDATE_PRODUCTS_SUCCESS, response.data));
+  } catch (err) {
+    console.log('AXIOS_ERROR', err.response);
+    dispatch(error(UPDATE_PRODUCTS_ERROR));
   }
 };
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -15,8 +15,28 @@ const addProduct = async product => {
   if (response.error) {
     throw new Error('dont created');
   }
+  return response;
 };
+
+const deleteProduct = async id => {
+  const response = await API.delete(`${URL_PRODUCTS}/${id}`);
+  if (response.error) {
+    throw new Error('dont deleted');
+  }
+  return response;
+};
+
+const updateProduct = async product => {
+  const response = await API.put(`${URL_PRODUCTS}/${product.id}`, product);
+  if (response.error) {
+    throw new Error('dont updated');
+  }
+  return response;
+};
+
 export default {
   getProducts,
   addProduct,
+  deleteProduct,
+  updateProduct,
 };
